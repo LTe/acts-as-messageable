db2ff136de54ad6ae371537bb88fd6d109f7cfbd
Piotr Niełacny <piotr.nielacny@gmail.com>
2012-12-10 11:54:44 +0100

    Add specs for ancestry methods [#35]

--- spec/custom-class_spec.rb
+++ spec/custom-class_spec.rb
@@ -5,18 +5,26 @@ class CustomMessage < ActsAsMessageable::Message
 end
 
 describe "custom class" do
-  before(:all) do
+  let(:alice) { User.find_by_email("alice@example.com") }
+  let(:bob)   { User.find_by_email("bob@example.com") }
+
+  before do
     User.acts_as_messageable :class_name => "CustomMessage"
+    @message = alice.send_message(bob, :topic => "Helou bob!", :body => "What's up?")
   end
 
   it "message should have CustomMessage class" do
-    message = @alice.send_message(@bob, :topic => "Helou bob!", :body => "What's up?")
-    message.class.should == CustomMessage
+    @message.class.should == CustomMessage
   end
 
   it "responds to custom_method" do
-    message = @alice.send_message(@bob, :topic => "Helou bob!", :body => "What's up?")
-    message.should respond_to(:custom_method)
+    @message.should respond_to(:custom_method)
+  end
+
+  it "return proper class with ancestry methods" do
+    @reply_message = @message.reply(:topic => "Re: Helou bob!", :body => "Fine!")
+    @reply_message.root.should == @message
+    @reply_message.root.class.should == CustomMessage
   end
 end
 
