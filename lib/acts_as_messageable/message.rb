7b63905e8f4b11223a3931c6a0b777923bbc9bd7
Piotr Niełacny <piotr.nielacny@toptal.com>
2022-12-19 18:18:18 +0100

    Add types for lib/acts_as_messageable/message.rb

--- lib/acts_as_messageable/message.rb
+++ lib/acts_as_messageable/message.rb
@@ -1,8 +1,10 @@
-# typed: true 
+# typed: strict 
 # frozen_string_literal: true
 
 module ActsAsMessageable
   class Message < ::ActiveRecord::Base
+    extend T::Sig
+
     include ActsAsMessageable::Scopes
     include Ancestry::InstanceMethods
 
@@ -11,9 +13,18 @@ module ActsAsMessageable
     belongs_to :received_messageable, polymorphic: true
     belongs_to :sent_messageable, polymorphic: true
 
+    sig { returns(T.nilable(T::Boolean)) }
     attr_accessor :removed, :restored
 
     cattr_accessor :required
+
+    sig { params(args: T.untyped, kwargs: T.untyped).void }
+    def initialize(*args, **kwargs)
+      @removed = T.let(false, T.nilable(T::Boolean))
+      @restored = T.let(false, T.nilable(T::Boolean))
+
+      super
+    end
 
     ActsAsMessageable.rails_api.new(self).attr_accessible(
       :topic, :body, :opened, :opened_at, :recipient_permanent_delete,
@@ -22,18 +33,21 @@ module ActsAsMessageable
     ActsAsMessageable.rails_api.new(self).default_scope('created_at desc')
 
     # @return [Boolean] whether the message has been read
+    sig { returns(T::Boolean) }
     def open?
       opened?
     end
 
     # @return [Boolean] whether the message has been read
     # @see open?
+    sig { returns(T::Boolean) }
     def opened?
       opened_at.present? || super
     end
 
     # Method open message (will mark message as read)
-    # @return [ActsAsMessageable::Message] current message
+    # @return [Boolean] whether the message has been open
+    sig { returns(T::Boolean) }
     def open
       ActsAsMessageable.rails_api.new(self).update_attributes!(opened_at: DateTime.now)
       ActsAsMessageable.rails_api.new(self).update_attributes!(opened: true)
@@ -43,7 +57,8 @@ module ActsAsMessageable
     alias read open
 
     # Method close message (will mark message as unread)
-    # @return [ActsAsMessageable::Message] current message
+    # @return [Boolean] whether the message has been closed
+    sig { returns(T::Boolean) }
     def close
       ActsAsMessageable.rails_api.new(self).update_attributes!(opened_at: nil)
       ActsAsMessageable.rails_api.new(self).update_attributes!(opened: false)
@@ -52,39 +67,46 @@ module ActsAsMessageable
     alias mark_as_unread close
     alias unread close
 
+    sig { returns(ActiveRecord::Base) }
     def from
       sent_messageable
     end
 
+    sig { returns(ActiveRecord::Base) }
     def to 
       received_messageable
     end
 
     # @param [ActiveRecord::Base] user
-    # @return [ActiveRecord::Associations::BelongsToAssociation] real receiver of the mssage
+    # @return [ActiveRecord::Base] real receiver of the mssage
+    sig { params(user: ActiveRecord::Base).returns(ActiveRecord::Base) }
     def real_receiver(user)
       user == from ? to : from
     end
 
     # @return [Boolean] whether user is participant of group message
     # @param [ActiveRecord::Base] user
+    sig { params(user: T.untyped).returns(T::Boolean) }
     def participant?(user)
       user.class.group_messages || (to == user) || (from == user)
     end
 
     # @return [Object] conversation tree
+    sig { returns(T.untyped) }
     def conversation
       root.subtree
     end
 
     # Method will mark message as removed
     # @return [TrueClass]
+    sig { returns(TrueClass) }
     def delete
       self.removed = true
     end
 
     # Method will mark message as removed
     # @return [TrueClass]
+    sig { returns(TrueClass) }
     def restore
       self.restored = true
     end
@@ -94,13 +116,16 @@ module ActsAsMessageable
     # @option args [String] topic Topic of the message
     # @option args [String] body Body of the message
     # @return [ActsAsMessageable::Message] a message that is a response to a given message
+    # @return [Boolean] when user is not participant of the message 
     # @see ActsAsMessageable::Model::InstanceMethods#reply_to
+    sig { params(args: T.any(T::Hash[String, String], String)).returns(T.any(ActsAsMessageable::Message, T::Boolean, ActiveRecord::Base)) }
     def reply(*args)
-      to.reply_to(self, *args)
+      T.unsafe(to).reply_to(self, *args)
     end
 
     # Method will return list of users in the conversation
     # @return [Array<ActiveRecord::Base>] users
+    sig { returns(T::Array[ActiveRecord::Base])}
     def people
       conversation.map(&:from).uniq!
     end
